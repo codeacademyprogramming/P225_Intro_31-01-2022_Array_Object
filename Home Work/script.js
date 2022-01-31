b258210ffec8239c43a56453aa8ca96844d09d23
HamidMammadov <hamidvm@code.edu.az>
2022-01-31 17:44:14 +0400

    First Commit

---
+++ Home Work/script.js
@@ -0,0 +1,113 @@
+//Home Work - 6
+// let a = parseInt(prompt("Birinci Deyeri Daxil Et"));
+// let b = parseInt(prompt("Ikinci Deyeri Daxil Et"));
+// let result = 0;
+// let min;
+// let max;
+
+// if(a > b)
+// {
+//     min = b;
+//     max = a;
+// }else if(b > a)
+// {
+//     min = a;
+//     max = b;
+// }else{
+//     console.log("Daxil Edilen Deyerler Berarber Olmasin");
+// }
+
+// while(min <= max)
+// {
+//     result+= min;
+//     min++;
+// }
+
+// console.log(result);
+
+//Home Work - 7
+
+// let m = parseInt(prompt("Ededi Daxil Et"));
+// let n = parseInt(prompt("Quvveti Deyeri Daxil Et"));
+// let result = 1;
+
+// if (n > 0) 
+// {
+//     while(n>0){
+//         result *= m;
+//         n--;
+//     }
+// }
+
+// console.log(result)
+
+// m = 5;
+// n = 3;
+
+// 1=1*5; 3=3-1
+// 5=5*5;  2=2-1
+// 25=25*5; 1=1-1
+
+
+//Array
+// let a1 = 5;
+// let a2 = 7;
+// let a3 = 9;
+
+// let arr = [5,7,9,15,17,5];
+
+// let a = arr[3];
+// console.log(a);
+// arr[3]+=7;
+// console.log(arr[3]);
+
+// console.log(arr[3]);
+// console.log(arr.length);
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log("Old "+arr[i]);
+//     arr[i]+=5;
+//     console.log("New "+arr[i]);
+// }
+
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[2]);
+// console.log(arr[3]);
+// console.log(arr[4]);
+// console.log(arr[5]);
+
+// let check = true;
+
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] == 25) {
+//         console.log("15 deyerinin indexi "+i);
+//         check = false;
+//         break;
+//     }
+// }
+
+// if (check) 
+// {
+//     console.log("Daxil Etdiyniz Deyer Arrayda Yoxdur");
+// }
+
+
+
+let stu1 = 
+{
+    name : "Vusal",
+    surName : "Aliyev",
+    group : "P225",
+    age : 20,
+    isMarred : false
+}
+
+let stu2 = 
+{
+    name : "Togrul",
+    surName : "HuseynAliyev",
+    group : "P225",
+    age : 18,
+    isMarred : false
+}
